a41afd0afff557bf982223201e5c948a6b01110d
The Tip Token Dev <jonathan.teel@thetiptoken.io>
2018-04-04 17:48:54 -0500

    Added modifier for crowdsale ending and allowing of ecoSystem address airdop tokens to be sent

--- contracts/TTTTokenSell.sol
+++ contracts/TTTTokenSell.sol
@@ -7,38 +7,38 @@ import "./TTTToken.sol";
 
 contract TTTTokenSell is Whitelist, Pausable {
 	using SafeMath for uint;
-	
+
 	// TTTToken contract address
 	address public tokenAddress;
 	address public wallet;
 	address public privatesaleAddress;
 	address public presaleAddress;
 	address public crowdsaleAddress;
-	
+
 	// Amount of wei currently raised
 	uint256 public weiRaised;
-	
+
 	// Variables for phase start/end
 	uint256 public startsAt;
 	uint256 public endsAt;
-	
+
 	enum CurrentPhase { Privatesale, Presale, Crowdsale }
-	
+
 	CurrentPhase currentPhase;
-	uint public currentPhaseRate;	
-	
+	uint public currentPhaseRate;
+
 	TTTToken public token;
-	
+
 	// Emitted at the end of each phase
 	event AmountRaised(address beneficiary, uint amountRaised);
 	// Emitted on token purchase
 	event TokenPurchase(address indexed purchaser, uint256 value, uint256 amount);
-	
+
 	modifier tokenPhaseIsActive() {
 		assert(now >= startsAt && now <= endsAt);
 		_;
 	}
-	
+
 	function TTTTokenSell(address _tokenAddress) {
 		wallet = 0xea2d2c0223af6e9c83db343aef2194564b27ee87;
 		// start sell at private sell
@@ -48,7 +48,7 @@ contract TTTTokenSell is Whitelist, Pausable {
 		crowdsaleAddress = 0xb6d40Fb512e7c824c6a33861b233eE50c263A950;
 		tokenAddress = _tokenAddress;
 	}
-	
+
 	function startPhase(uint _phase, uint256 _startsAt, uint256 _endsAt) external onlyOwner {
 		require(_phase >= 0 && _phase <= 2);
 		require(_startsAt > endsAt && _endsAt > _startsAt);
@@ -58,7 +58,7 @@ contract TTTTokenSell is Whitelist, Pausable {
 		startsAt = _startsAt;
 		endsAt = _endsAt;
 	}
-	
+
 	function buyTokens(address _to) tokenPhaseIsActive onlyWhitelisted whenNotPaused payable {
 		require(_to != 0x0);
 		require(msg.value > 0);
@@ -70,13 +70,13 @@ contract TTTTokenSell is Whitelist, Pausable {
 		wallet.transfer(weiAmount);
 		token.transferFromTokenSell(_to, from, tokens);
 	}
-	
+
 	// To contribute, send a value transaction to the token sell Address.
-    // Please include at least 100 000 gas.
-    function () payable {
-        buyTokens(msg.sender);
+	// Please include at least 100 000 gas.
+	function () payable {
+		buyTokens(msg.sender);
 	}
-	
+
 	function getPhaseAddress() internal returns (address phase) {
 		if(currentPhase == CurrentPhase.Privatesale)
 			return privatesaleAddress;
@@ -86,7 +86,7 @@ contract TTTTokenSell is Whitelist, Pausable {
 			return crowdsaleAddress;
 		return 0x0;
 	}
-	
+
 	// Amount of TTT per 1 ether. Will be updated closed to deployment
 	function getPhaseRate() internal returns (uint rate) {
 		if(currentPhase == CurrentPhase.Privatesale)
